2c2faaf60171bff3ded5fa4bab1192695f6bbeca
fb00s <153862225+fb00s@users.noreply.github.com>
2024-08-30 17:47:14 -0300

    Add files via upload

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Felipe Boos
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void mudarNumero(int numero){
+    }
+    
+}
